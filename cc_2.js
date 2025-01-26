5f779beab3d2e3d5de32c7f710d1a710679b6189
Fernando <mhfernando778@gmail.com>
2025-01-26 12:21:13 -0500

    Task 1 - Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,7 @@
+// Task 1: Store Inventory //
+let products = ["Toy", "Dress", "Suit", "Shoes", "Jeans"]; // 5 original products
+products.push("Chains"); // adding new product
+let removed = products.pop(); // removing last item from the array
+
+console.log(products); // logs the updated array after adding new product
+console.log(removed); // logs the updated array after removing last product
