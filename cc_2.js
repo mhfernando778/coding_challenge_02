efe5bc7cced2d6b40d6a8abe031680fffeb2a449
Fernando <mhfernando778@gmail.com>
2025-01-26 13:39:44 -0500

    Task 2 - Student Scores

--- cc_2.js
+++ cc_2.js
@@ -5,3 +5,12 @@ let removed = products.pop(); // removing last item from the array
 
 console.log(products); // logs the updated array after adding new product
 console.log(removed); // logs the updated array after removing last product
+
+// Task 2: Student Scores //
+let scores = [75, 90, 82, 95, 70]; // an array of 5 students scores
+scores[1] = 100; // the second score updated to 100
+let total = scores.reduce((sum, scores) => sum + scores, 0); // adding all the scores together
+let averageScores = total / scores.length; // dividing the total by the length to find the average score
+
+console.log(scores); // log of updated array of scores
+console.log(averageScores); // log of average of all scores
